b8d361e34fb5e58c861dd9f1f1aa0cbcf2d17f07
kevininlab <Administrator@CQ5EPSR-PC21>
2025-09-04 11:38:56 +0700

    add login validation

---
+++ study/Validation/LoginValidation.cs
@@ -0,0 +1,15 @@
+﻿namespace study.Validation;
+using study.Model.DTOs;
+using FluentValidation;
+public class LoginValidation : AbstractValidator<LoginRequest>
+{
+    public LoginValidation()
+    {
+        RuleFor(user => user.Username)
+            .NotEmpty().WithMessage("Username is required.");
+
+        RuleFor(user => user.Password)
+            .NotEmpty().WithMessage("Password is required.");
+    }
+}
+
